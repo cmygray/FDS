c84db1aa049689fd253da0d7b55b12b55e35597c
cmygray <cmygray@gmail.com>
2017-11-26 16:41:18 +0900

    서버 폴더 정리

--- todo/ver.1.0/server.js
+++ todo/ver.1.0/server.js
@@ -5,9 +5,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const cors = require('cors')
+
 const app = express();
 const port = process.env.PORT || 4500;
 
+app.use(cors());
 // Static File Service
 app.use(express.static('public'));
 
